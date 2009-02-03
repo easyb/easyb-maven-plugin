b2606b750b49c9484872f58e8c41b55a66660300
rfciii <rfciii@9bebd20b-ac44-0410-9e34-335f4663fabf>
2009-02-03 22:53:20 +0000

    Issue 130 fixing problem with the maven plugin failing with stories containing narratives
    git-svn-id: http://easyb.googlecode.com/svn/trunk@597 9bebd20b-ac44-0410-9e34-335f4663fabf

---
+++ src/it/narrative-test/src/test/easyb/NarrativeStory.groovy
@@ -0,0 +1,11 @@
+narrative "description", {
+    as_a "role"
+    i_want "something"
+    so_that "something else"
+}
+
+scenario "stack", {
+    given "an empty stack"
+    when "null is pushed"
+    then "then the stack should still be empty"
+}
