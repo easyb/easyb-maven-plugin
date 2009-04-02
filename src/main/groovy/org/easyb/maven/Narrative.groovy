f9adc67a3eb9adcd3a48fb5c1920f765c95a30bc
rfciii <rfciii@9bebd20b-ac44-0410-9e34-335f4663fabf>
2009-04-02 20:01:26 +0000

    Removing unused files
    git-svn-id: http://easyb.googlecode.com/svn/trunk@681 9bebd20b-ac44-0410-9e34-335f4663fabf

--- src/main/groovy/org/easyb/maven/Narrative.groovy
+++
@@ -1,7 +0,0 @@
-package org.easyb.maven
-
-public class Narrative {
-    String as_a
-    String i_want
-    String so_that
-}
--- src/main/groovy/org/easyb/maven/Scenario.groovy
+++
@@ -1,9 +0,0 @@
-package org.easyb.maven
-
-class Scenario {
-    String name
-    String result
-    List givens
-    List whens
-    List thens
-}
