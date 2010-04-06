93aa617dd8e67e369cd067551838319c1398e2ba
richard.vowles <richard.vowles@9bebd20b-ac44-0410-9e34-335f4663fabf>
2010-04-06 22:29:34 +0000

    Validation of the parallel test assumed they would finish in order, and they sometimes don't. This new validation now just holds an array of stories that need to complete and pattern matches to pick them up.
    git-svn-id: http://easyb.googlecode.com/svn/trunk@934 9bebd20b-ac44-0410-9e34-335f4663fabf

--- src/it/parallel-test/validate.groovy
+++ src/it/parallel-test/validate.groovy
@@ -6,15 +6,22 @@ def results = new XmlParser().parse(report)
 assert '5' == results.'@totalbehaviors'
 assert '0' == results.'@totalfailedbehaviors'
 
+def stories = ['1', '2', '3', '4', '5']
+def pattern = ~/Running long[12345] story/
+
 // Verify ran in parallel
 log = new File("${basedir}/build.log")
 log.withReader { reader ->
     String line = reader.readLine()
-    while (!line.contains('Running long1 story')) {
-        line = reader.readLine()
+
+    // allows us to read them if they complete out of order
+    while ( line && !line.startsWith("ERR:") && stories.size() > 0 ) {
+      def matcher = pattern.matcher(line)
+      if ( matcher.find() ) {
+        stories.remove( line.substring(matcher.start()+12, matcher.start()+13) )
+      }
+      line = reader.readLine()
     }
 
-    (2..5).each { n ->
-        assert reader.readLine().contains("Running long$n story")
-    }
+    assert stories.size() == 0
 }
