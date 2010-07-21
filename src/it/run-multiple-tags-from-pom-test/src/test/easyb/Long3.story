83b79f61b3b727617ad2df2ebbdbbc9123ea1831
jeffrey.erikson <jeffrey.erikson@9bebd20b-ac44-0410-9e34-335f4663fabf>
2010-07-21 00:00:32 +0000

    test to make sure Maven plugin can handle running multiple tags
    git-svn-id: http://easyb.googlecode.com/svn/trunk@1043 9bebd20b-ac44-0410-9e34-335f4663fabf

---
+++ src/it/run-multiple-tags-from-pom-test/src/test/easyb/Long3.story
@@ -0,0 +1,6 @@
+tags "runmetoo"
+scenario "some cool feature", {
+  given "some context"
+  when "some event occurs"
+  then "some outcome is triggered"
+}
---
+++ src/it/run-multiple-tags-from-pom-test/validate.groovy
@@ -0,0 +1,27 @@
+// Verify behavior report
+report = new File("${basedir}/target/easyb-report.xml")
+assert report.exists()
+
+def results = new XmlParser().parse(report)
+assert '3' == results.'@totalbehaviors'
+assert '0' == results.'@totalfailedbehaviors'
+
+def stories = ['1', '2', '3']
+def pattern = ~/Running long[12345] story/
+
+// Verify ran in parallel
+log = new File("${basedir}/build.log")
+log.withReader { reader ->
+    String line = reader.readLine()
+
+    // allows us to read them if they complete out of order
+    while ( line && !line.startsWith("ERR:") && stories.size() > 0 ) {
+      def matcher = pattern.matcher(line)
+      if ( matcher.find() ) {
+        stories.remove( line.substring(matcher.start()+12, matcher.start()+13) )
+      }
+      line = reader.readLine()
+    }
+
+    assert stories.size() == 0
+}
